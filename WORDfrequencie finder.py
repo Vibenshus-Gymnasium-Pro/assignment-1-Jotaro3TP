d1d05eb78e6d94b47af97415d5b3c9fc3aaff34e
Josef <110970040+Jotaro3TP@users.noreply.github.com>
2023-01-25 23:19:31 +0100

    Add files via upload

--- WORDfrequencie finder.py
+++ WORDfrequencie finder.py
@@ -5,14 +5,17 @@ import matplotlib.pyplot as plt
 
 frequency = {}
 document_text = open('test.txt', 'r')
-text_string = document_text.read().lower()
-match_pattern = re.findall(r'\b[a-z]{3,15}\b', text_string)
+textinstring = document_text.read().lower()
+findwords = re.findall(r'\b[a-z]{3,15}\b', textinstring)
 
-for word in match_pattern:
+for word in findwords:
     count = frequency.get(word, 0)
     frequency[word] = count + 1
 most_frequent = dict(sorted(frequency.items(), key=lambda elem: elem[1], reverse=True))
 most_frequent_count = most_frequent.keys()
+
+plt.bar(list(most_frequent.keys()), most_frequent.values(), color='g')
+plt.show()
 
 for words in most_frequent_count:
     print(words, most_frequent[words])
